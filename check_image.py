063b20f5735ad48483859b8b02fc3aac924ac117
NeuralNetNinja1729 <vishweshpatel2004@gmail.com>
2024-09-21 01:52:12 -0400

    Update check_image.py

--- check_image.py
+++ check_image.py
@@ -1,18 +1,20 @@
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+
 model = tf.keras.models.load_model("./models/digit_classification_cnn_with_BatchNormalizationAndDropout_2.h5")
 
-image_path = "./images/8.jpeg" 
-image = Image.open(image_path).convert("L") 
+path = "./images/image_8.png" 
+image = Image.open(path).convert("L") 
 
  
 image = image.resize((32, 32))
-image_array = np.array(image)
-image_array = image_array
-# print(image_array[16])
-image_array = image_array.reshape((1, 32, 32, 1))
-predictions = model.predict(image_array)
-predicted_class = np.argmax(predictions, axis=1)
 
-print(f"Predicted Digit: {predicted_class[0]}")
+image_array = np.array(image).reshape((1, 32, 32, 1))
+
+predictions = model.predict(image_array)
+predicted_class = np.argmax(predictions, axis=1)[0]
+confidence_score = np.max(predictions, axis=1)[0]
+
+print(f"Predicted Digit: {predicted_class}.")
+print(f'Confidence Score: {round(confidence_score, 2)}')
